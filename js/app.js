924d5e7b2e9ebfca1e15dbe9062897e0caed862d
Денис <corvus007@yandex.ru>
2017-03-20 21:28:28 +0300

    Упрощен js код для переключения навигации

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {
-  let toggleMenu = document.querySelector('.top-menu__toggle');
-  let mainNav = document.querySelector('.main-nav');
+window.addEventListener('DOMContentLoaded', function() {
+  var toggleMenu = document.querySelector('.top-menu__toggle');
+  var mainNav = document.querySelector('.main-nav');
   toggleMenu.classList.remove('top-menu__toggle--no-js');
   mainNav.classList.remove('main-nav--no-js');
 
-  toggleMenu.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (toggleMenu.classList.contains('top-menu__toggle--closed')) {
-      toggleMenu.classList.remove('top-menu__toggle--closed');
-      mainNav.classList.remove('main-nav--closed');
-      toggleMenu.classList.add('top-menu__toggle--opened');
-      mainNav.classList.add('main-nav--opened');
-    } else {
-      toggleMenu.classList.remove('top-menu__toggle--opened');
-      mainNav.classList.remove('main-nav--opened');
-      toggleMenu.classList.add('top-menu__toggle--closed');
-      mainNav.classList.add('main-nav--closed');
-    }
-  });
+  if (toggleMenu) {
+    toggleMenu.addEventListener('click', function(event) {
+      event.preventDefault();
+      toggleMenu.classList.toggle('top-menu__toggle--closed');
+      mainNav.classList.toggle('main-nav--closed');
+    });
+  }
 });
