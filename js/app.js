2b0c991b7f827aebc27dc7c8f5bc1688f5047f26
Денис <corvus007@yandex.ru>
2017-03-23 18:48:17 +0300

    Создаение отзывчивого дизайна на главной странице

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var toggleMenu = document.querySelector('.top-menu__toggle');
+  var toggleMenu = document.querySelector('.main-nav__toggle');
   var mainNav = document.querySelector('.main-nav');
-  toggleMenu.classList.remove('top-menu__toggle--no-js');
   mainNav.classList.remove('main-nav--no-js');
 
   if (toggleMenu) {
     toggleMenu.addEventListener('click', function(event) {
       event.preventDefault();
-      toggleMenu.classList.toggle('top-menu__toggle--closed');
       mainNav.classList.toggle('main-nav--closed');
     });
   }
