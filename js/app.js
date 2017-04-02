5e645e7ee5b8504b89641005409152f42f3787aa
Денис <corvus007@yandex.ru>
2017-04-02 23:18:56 +0300

    Добавлет открытие модального окна

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
 window.addEventListener('DOMContentLoaded', function() {
   var toggleMenu = document.querySelector('.main-nav__toggle');
   var mainNav = document.querySelector('.main-nav');
+
+  var modal = document.querySelector('.modal');
+  var triggerModalItems = document.querySelectorAll('.js-trigger-modal');
+
   mainNav.classList.remove('main-nav--no-js');
+  modal.classList.remove('modal--no-js');
 
   if (toggleMenu) {
     toggleMenu.addEventListener('click', function(event) {
       event.preventDefault();
       mainNav.classList.toggle('main-nav--closed');
+    });
+  }
+
+  if (modal) {
+    Array.prototype.forEach.call(triggerModalItems, function(triggerModal) {
+      console.log(triggerModal)
+      triggerModal.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.classList.toggle('modal--closed');
+      });
+    });
+
+    modal.addEventListener('click', function(event) {
+      modal.classList.toggle('modal--closed');
     });
   }
 });
