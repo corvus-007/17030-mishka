1f50570338f0bdd6914bfeb9de1369f681292c58
Денис <corvus007@yandex.ru>
2017-04-06 20:39:09 +0300

    Добавляет возможность переключать кастомные чекбоксы и радиобаттоны

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ window.addEventListener('DOMContentLoaded', function() {
   var modal = document.querySelector('.modal');
   var triggerModalItems = document.querySelectorAll('.js-trigger-modal');
 
+  var formOrder = document.forms['form-order'];
+  var customLabel = null;
+
   mainNav.classList.remove('main-nav--no-js');
 
   if (toggleMenu) {
@@ -26,6 +29,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     modal.addEventListener('click', function(event) {
       modal.classList.toggle('modal--closed');
+    });
+  }
+
+  if (formOrder) {
+    formOrder.addEventListener('keypress', function(event) {
+      customLabel = event.target;
+      if (event.keyCode === 13 && customLabel.hasAttribute('tabindex')) {
+        customLabel.control.checked = (customLabel.control.checked) ? false : true;
+      }
     });
   }
 });
